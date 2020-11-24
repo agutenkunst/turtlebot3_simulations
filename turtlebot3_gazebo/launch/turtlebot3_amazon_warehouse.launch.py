a0ed75a2e23303d212d2335bce931005ff988c5e
Alexander Gutenkunst <alex.gutenkunst+github.com>
2020-11-24 20:45:00 +0100

    Add launchfile for amazon warehouse

---
+++ turtlebot3_gazebo/launch/turtlebot3_amazon_warehouse.launch.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+#
+# Copyright 2019 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Darby Lim, Ryan Shim
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
+from launch.actions import ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+
+TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+
+    ld = LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([launch_file_dir, '/robot_state_publisher.launch.py']),
+            launch_arguments={'use_sim_time': use_sim_time}.items(),
+        ),
+    ])
+
+    warehouse_pkg_dir = get_package_share_directory('aws_robomaker_small_warehouse_world')
+    warehouse_launch_path = os.path.join(warehouse_pkg_dir, 'launch')
+
+    warehouse_world_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([warehouse_launch_path, '/no_roof_small_warehouse_launch.py'])
+    )
+
+    ld.add_action(warehouse_world_cmd)
+
+    return ld
